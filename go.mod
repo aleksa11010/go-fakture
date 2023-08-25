ff064337154fb186fd10842854331a8c833c2190
Aleksa Arsic <aleksa.arsic@harness.io>
2023-08-25 13:31:28 +0200

    changes module name

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/angelodlfrtr/go-invoice-generator
+module github.com/aleksa11010/go-fakture
 
 go 1.17
 
--- options.go
+++ options.go
@@ -7,32 +7,32 @@ type UnicodeTranslateFunc func(string) string
 type Options struct {
 	AutoPrint bool `json:"auto_print,omitempty"`
 
-	CurrencySymbol    string `default:"€ " json:"currency_symbol,omitempty"`
+	CurrencySymbol    string `default:"RSD " json:"currency_symbol,omitempty"`
 	CurrencyPrecision int    `default:"2" json:"currency_precision,omitempty"`
 	CurrencyDecimal   string `default:"." json:"currency_decimal,omitempty"`
 	CurrencyThousand  string `default:" " json:"currency_thousand,omitempty"`
 
-	TextTypeInvoice      string `default:"INVOICE" json:"text_type_invoice,omitempty"`
+	TextTypeInvoice      string `default:"FAKTURA" json:"text_type_invoice,omitempty"`
 	TextTypeQuotation    string `default:"QUOTATION" json:"text_type_quotation,omitempty"`
 	TextTypeDeliveryNote string `default:"DELIVERY NOTE" json:"text_type_delivery_note,omitempty"`
 
-	TextRefTitle         string `default:"Ref." json:"text_ref_title,omitempty"`
-	TextVersionTitle     string `default:"Version" json:"text_version_title,omitempty"`
-	TextDateTitle        string `default:"Date" json:"text_date_title,omitempty"`
-	TextPaymentTermTitle string `default:"Payment term" json:"text_payment_term_title,omitempty"`
+	TextRefTitle         string `default:"Referenca" json:"text_ref_title,omitempty"`
+	TextVersionTitle     string `default:"Verzija" json:"text_version_title,omitempty"`
+	TextDateTitle        string `default:"Datum" json:"text_date_title,omitempty"`
+	TextPaymentTermTitle string `default:"Rok za placanje" json:"text_payment_term_title,omitempty"`
 
-	TextItemsNameTitle     string `default:"Name" json:"text_items_name_title,omitempty"`
-	TextItemsUnitCostTitle string `default:"Unit price" json:"text_items_unit_cost_title,omitempty"`
-	TextItemsQuantityTitle string `default:"Qty" json:"text_items_quantity_title,omitempty"`
-	TextItemsTotalHTTitle  string `default:"Total no tax" json:"text_items_total_ht_title,omitempty"`
-	TextItemsTaxTitle      string `default:"Tax" json:"text_items_tax_title,omitempty"`
-	TextItemsDiscountTitle string `default:"Discount" json:"text_items_discount_title,omitempty"`
-	TextItemsTotalTTCTitle string `default:"Total" json:"text_items_total_ttc_title,omitempty"`
+	TextItemsNameTitle     string `default:"Naziv" json:"text_items_name_title,omitempty"`
+	TextItemsUnitCostTitle string `default:"Cena" json:"text_items_unit_cost_title,omitempty"`
+	TextItemsQuantityTitle string `default:"Kol." json:"text_items_quantity_title,omitempty"`
+	TextItemsTotalHTTitle  string `default:"Ukupno bez poreza" json:"text_items_total_ht_title,omitempty"`
+	TextItemsTaxTitle      string `default:"Porez" json:"text_items_tax_title,omitempty"`
+	TextItemsDiscountTitle string `default:"Popust" json:"text_items_discount_title,omitempty"`
+	TextItemsTotalTTCTitle string `default:"Ukupno" json:"text_items_total_ttc_title,omitempty"`
 
-	TextTotalTotal      string `default:"TOTAL" json:"text_total_total,omitempty"`
-	TextTotalDiscounted string `default:"TOTAL DISCOUNTED" json:"text_total_discounted,omitempty"`
-	TextTotalTax        string `default:"TAX" json:"text_total_tax,omitempty"`
-	TextTotalWithTax    string `default:"TOTAL WITH TAX" json:"text_total_with_tax,omitempty"`
+	TextTotalTotal      string `default:"Ukupno" json:"text_total_total,omitempty"`
+	TextTotalDiscounted string `default:"Ukupno popust" json:"text_total_discounted,omitempty"`
+	TextTotalTax        string `default:"Porez" json:"text_total_tax,omitempty"`
+	TextTotalWithTax    string `default:"Ukupno sa porezom" json:"text_total_with_tax,omitempty"`
 
 	BaseTextColor []int `default:"[35,35,35]" json:"base_text_color,omitempty"`
 	GreyTextColor []int `default:"[82,82,82]" json:"grey_text_color,omitempty"`
